e935a48a0e0a03694b4663699880dcf14eef5d03
Obaid <obaid8712@gmail.com>
2019-03-03 09:11:20 -0500

    Variable Name Change

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,38 +15,39 @@ with open(budgetCSV, 'r') as csvfile:
     # Loop through the data 
     i=0
     # Declare a place holder for numeric data with out header
-    numData0=[]
-    numData=[]
+    MonthsData=[]
+    NetData=[]
     for row in csvreader:    
-        i = i +1
-        if i != 1 :   
-            numData0.append(row[0])         
+        i += 1
+        if i != 1 : 
+            #Add all months data to placeholder for calculation  
+            MonthsData.append(row[0])         
             #Add profit and loos data to placeholder for calculation
-            numData.append(row[1])   
+            NetData.append(row[1])   
     # Total number of months included in the dataset
-    numMonth=len(numData)
+    numMonth=len(MonthsData)
     # Net total amount of "Profit/Losses" over the entire period
     netProfit=0
-    for x in numData:
+    for x in NetData:
         netProfit=netProfit+int(x)
     # The average of the changes in "Profit/Losses" over the entire period
     i=1
-    bigNumData=numData[0]
+    bigNumData=NetData[0]
     sumChange=0
     bigIncrease=0
     bigDecrease=0
-    for x in numData: 
+    for x in NetData: 
         if i < numMonth : 
-            Increse = int(numData[i])-int(x)  
-            sumChange=sumChange + Increse
-            # The greatest increase in profits (amount) over the entire period
-            if Increse > bigIncrease:
-                bigIncrease=Increse               
-                bigIncDay=numData0[i]
+            Change = int(NetData[i])-int(x)  
+            sumChange=sumChange + Change
+            # The greatest Change in profits (amount) over the entire period
+            if Change > bigIncrease:
+                bigIncrease=Change               
+                bigIncMonth=MonthsData[i]
             #The greatest decrease in losses (date and amount) over the entire period
-            if Increse < bigDecrease:
-                bigDecrease=Increse               
-                bigDecDay=numData0[i]
+            if Change < bigDecrease:
+                bigDecrease=Change               
+                bigDecMonth=MonthsData[i]
             i+=1    
 
     # Calculate Average Change
@@ -56,12 +57,10 @@ with open(budgetCSV, 'r') as csvfile:
     print(f"------------------------------")
     print(f"Total months : {numMonth}")
     print(f"Total : {netProfit}")
-    #for x in numData:
-    #    print(f"data {x}")
-    #print(f"Average  Change: {avgChange}")
+       
     print(f"Average  Change: {'${:,.2f}'.format(avgChange)}")
-    print(f"Greatest Increase in Profits: {bigIncDay} {'${:}'.format(bigIncrease)}")
-    print(f"Greatest Decrease in Profits: {bigDecDay} {'${:}'.format(bigDecrease)}")
+    print(f"Greatest Increase in Profits: {bigIncMonth} {'${:}'.format(bigIncrease)}")
+    print(f"Greatest Decrease in Profits: {bigDecMonth} {'${:}'.format(bigDecrease)}")
     #print(f"Big Increase day {bigIncDay}")
 
 # Specify the file to write to
@@ -74,6 +73,6 @@ with open(output_path, 'w') as text_file:
     print(f"Total months : {numMonth}",file=text_file)
     print(f"Total : {netProfit}",file=text_file)
     print(f"Average  Change: {'${:,.2f}'.format(avgChange)}",file=text_file)
-    print(f"Greatest Increase in Profits: {bigIncDay} {'${:}'.format(bigIncrease)}",file=text_file)
-    print(f"Greatest Decrease in Profits: {bigDecDay} {'${:}'.format(bigDecrease)}",file=text_file)
+    print(f"Greatest Increase in Profits: {bigIncMonth} {'${:}'.format(bigIncrease)}",file=text_file)
+    print(f"Greatest Decrease in Profits: {bigDecMonth} {'${:}'.format(bigDecrease)}",file=text_file)
 
